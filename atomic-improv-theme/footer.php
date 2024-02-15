0103e1410aad49477acc6275a5f8f1c039f14fc3
Kayla Jolie <kyle.jolie.ab@gmail.com>
2024-02-15 14:58:08 -0700

    set up a very basic header. little styling other than getting flex working.

--- atomic-improv-theme/footer.php
+++ atomic-improv-theme/footer.php
@@ -12,19 +12,22 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'atomic-improv' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'atomic-improv' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'atomic-improv' ), 'atomic-improv', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
+		<div class="container">
+
+			<div class="site-info">
+				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'atomic-improv' ) ); ?>">
+					<?php
+					/* translators: %s: CMS name, i.e. WordPress. */
+					printf( esc_html__( 'Proudly powered by %s', 'atomic-improv' ), 'WordPress' );
+					?>
+				</a>
+				<span class="sep"> | </span>
+					<?php
+					/* translators: 1: Theme name, 2: Theme author. */
+					printf( esc_html__( 'Theme: %1$s by %2$s.', 'atomic-improv' ), 'atomic-improv', '<a href="http://underscores.me/">Underscores.me</a>' );
+					?>
+			</div><!-- .site-info -->
+		</div><!-- .container -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
---
+++ atomic-improv-theme/front-page.php
@@ -0,0 +1,22 @@
+<?php 
+
+  // This is the template for displaying the custom landing page.
+
+  // @package Atomic_Improv
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+    <section>
+      <div class="container">
+
+        <h1>Atomic Improv</h1>
+        <p>This will be our home page. Testing if this is working.</p>
+      </div><!-- .container -->
+    </section>
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
