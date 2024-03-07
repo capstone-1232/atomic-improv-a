2538ac69907c00bcfea6c55d0c353f9f23904911
Kayla Jolie <kyle.jolie.ab@gmail.com>
2024-03-07 14:32:13 -0700

    More html done
    Not 100% done but 3-4 pages down

--- atomic-improv-theme/functions.php
+++ atomic-improv-theme/functions.php
@@ -190,3 +190,23 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
+// https://codeastrology.com/how-to-add-meta-descriptions-in-wordpress-without-plugin/#How_to_Add_Meta_Descriptions_in_WordPress_Without_Plugin
+// This code, copied from above, should give meta descriptions to all pages and posts.
+// function codeastrology_meta_description() {
+//     global $post;
+//     if ( is_singular() ) {
+//         $des_post = strip_tags( $post->post_content );
+//         $des_post = strip_shortcodes( $post->post_content );
+//         $des_post = str_replace( array("\n", "\r", "\t"), ' ', $des_post );
+//         $des_post = mb_substr( $des_post, 0, 300, 'utf8' );
+//         echo '<meta name="description" content="' . $des_post . '" />' . "\n";
+//     }
+//     if ( is_home() ) {
+//         echo '<meta name="description" content="' . get_bloginfo( "description" ) . '" />' . "\n";
+//     }
+//     if ( is_category() ) {
+//         $des_cat = strip_tags(category_description());
+//         echo '<meta name="description" content="' . $des_cat . '" />' . "\n";
+//     }
+// }
+// add_action( 'wp_head', 'codeastrology_meta_description');
