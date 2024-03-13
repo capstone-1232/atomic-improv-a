a4e20ed2aad60d3c2f7bb82867583db2be9af933
Kayla Jolie <kyle.jolie.ab@gmail.com>
2024-03-13 13:21:45 -0600

    added success page

--- atomic-improv-theme/page-shows.php
+++ atomic-improv-theme/page-shows.php
@@ -1,21 +1,65 @@
-<?php 
-
-  // This is the template for the About Us page.
-
-  // @package Atomic_Improv
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Atomic_Improv
+ */
 
 get_header();
 ?>
 
-	<main id="primary" class="site-main">
-    <section>
-      <div class="container">
 
-        <h1>Public Shows</h1>
-        <p>Where you can see us LIVE!</p>
-      </div><!-- .container -->
-    </section>
-	</main><!-- #main -->
+<main id="primary" class="site-main">
+  <section>
+    <div class="container">
+
+      <h1>
+        <?php echo get_the_title(); ?>
+      </h1>
+
+      <p>This page is just for specific types of posts, the ones regarding PUBLIC SHOWS that anyone can attend</p>
+      <!-- start loop -->
+      <?php
+      $args = array(
+        'post_type' => 'post'
+      );
+      $the_query = new WP_Query($args);
+      // Tests if there even are any posts
+      if ($the_query->have_posts()):
+        // As long as there are new posts...
+        while ($the_query->have_posts()):
+          // Select the next post
+          $the_query->the_post();
+          ?>
+          <h2>
+            <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+              <?php the_title(); ?>
+            </a>
+          </h2>
+          <p>Hi I am a post</p>
+          <?php the_content(); ?>
+
+        <?php endwhile; else: ?>
+        <!-- shows this if there are no posts -->
+        <p>
+          <?php esc_html_e("Sorry, no posts matched your criteria."); ?>
+        </p>
+        <!-- end loop -->
+      <?php
+      endif;
+      wp_reset_postdata();
+      ?>
+
+    </div><!-- .container -->
+  </section>
+</main><!-- #main -->
 
 <?php
 get_footer();
