165691cb6cf7498e3b779187634a489048464590
Jen <47681735+j-soule@users.noreply.github.com>
2024-03-22 13:24:29 -0600

    Community page
    Updated community section to match revisions, added Riverhawks logo

--- atomic-improv-theme/page-community.php
+++ atomic-improv-theme/page-community.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying all pages
  *
@@ -21,10 +22,21 @@ get_header();
     <div class="container">
       <h2>In the community</h2>
 
-      <div><a href="https://worldjrfootballchampionships.com/"><img src="" alt="football alberta logo"></a> <p></p></div>
-      <div><a href="https://greatoutdoorscomedyfestival.com/"><img src="" alt="great outdoor comedy fest"></a> <p></p></div>
-      <div><a href="https://icedistrict.com/event/oktoberfest-2023/"><img src="" alt="oktoberfest logo"></a> <p></p></div>
-      <div><a href="https://riverhawksbaseball.com/"><img src="" alt="edmonton riverhawks logo"></a> <p></p></div>
+      <div class="card"><a href="https://worldjrfootballchampionships.com/"><img src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/football-alberta-logo.webp" alt="football alberta logo"></a>
+        <p>You can catch Donovan as he helps entertain and host fans and players for another great international event in our city. If baseball isn’t enough Donovan will also be working hard on stage at the fan tailgate party for the U20 international football championships.</p>
+      </div>
+      <div class="card"><a href="https://greatoutdoorscomedyfestival.com/"><img src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/gocf-logo.webp" alt="great outdoor comedy fest"></a>
+        <p>Donovan is hitting the road as the head park ranger hosting the wild successful outdoor comedyfest all across Canada this summer.
+
+        </p>
+      </div>
+      <div class="card"><a href="https://icedistrict.com/event/oktoberfest-2023/"><img src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/oktoberfest-logo.webp" alt="oktoberfest logo"></a>
+        <p>PROST!!!
+          Join Donovan as he hosts Canada’s second largest Octoberfest at Fan Park for the Edmonton oilers entertainment group, tons of great bands and giveaways.  Don’t forget your Lederhosen!</p>
+      </div>
+      <div class="card"><a href="https://riverhawksbaseball.com/"><img src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/riverhawks-logo.webp" alt="edmonton riverhawks logo"></a>
+        <p>As summer hits you can see Donovan at another Home Run event as the infield host for the WCL Edmonton Riverhawks Baseball team right in the heart of the river valley at Re/Max field. Help Donovan Cheer, doing and win both on and off the field.  Tons of great giveaways so bring your team spirit June-August.</p>
+      </div>
     </div>
   </section>
   <section>
@@ -42,12 +54,12 @@ get_header();
       );
       $the_query = new WP_Query($args);
       // Tests if there even are any posts
-      if ($the_query->have_posts()):
+      if ($the_query->have_posts()) :
         // As long as there are new posts...
-        while ($the_query->have_posts()):
+        while ($the_query->have_posts()) :
           // Select the next post
           $the_query->the_post();
-          ?>
+      ?>
           <h2>
             <a href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
               <?php the_title(); ?>
@@ -56,7 +68,8 @@ get_header();
           <p>Hi I am a post</p>
           <?php the_content(); ?>
 
-        <?php endwhile; else: ?>
+        <?php endwhile;
+      else : ?>
         <!-- shows this if there are no posts -->
         <p>
           <?php esc_html_e("Sorry, no posts matched your criteria."); ?>
