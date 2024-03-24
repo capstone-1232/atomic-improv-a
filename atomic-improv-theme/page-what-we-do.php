ec8f69a8a3616ea70db9a0cedbd8548fbfdab6da
Jen <47681735+j-soule@users.noreply.github.com>
2024-03-24 10:11:46 -0600

    fixed markup on that funny santa

--- atomic-improv-theme/page-what-we-do.php
+++ atomic-improv-theme/page-what-we-do.php
@@ -11,18 +11,19 @@ get_header();
   <section class="corporate-events">
     <div class="container">
 
-      <h2>What We Do</h2>
+      <h3>Corporate Events</h3>
       <img
         src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/poy-donovan-chris-sm.webp"
         alt="donovan and chris posing">
       <p>Our interactive, custom and hilarious shows are the perfect fit for any audience. </p>
       <p>Fast, funny and tailored to the crowd, Atomic Improv is always a hit.</p>
       <p>You're not just watching the show … you're a part of it!</p>
+      <a href="contact-us">Book Now</a>
     </div><!-- .container -->
   </section>
   <section class="power-of-yes">
     <div class="container">
-      <h2>Power of Yes</h2>
+      <h3>Power of Yes</h3>
       <a href="https://powerofyes.ca/"><img
         src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/power-of-yes-sm.webp"
         alt="Power of Yes Logo"></a>
@@ -37,12 +38,16 @@ get_header();
       <p>Whether you have a team of 10 or an audience of 1,000, our workshop adapts to fit your needs. Choose from a
         hands-on session for intimate teams or turn it into an interactive and entertaining keynote address for a larger
         crowd.</p>
-      <p>Additional Booking Options for Donovan and Chris:</p>
+        <a href="contact-us">Book Now</a>
+      
+      
+    </div>
+    <div>
+    <p>Additional Booking Options for Donovan and Chris:</p>
       <ul>
         <li>Professional MC's: Corporate, Conference, and Special Events</li>
         <li>Special Package Pricing when booking Professional MC's PLUS Power of Yes! workshop</li>
       </ul>
-      
     </div>
     <div>
       <!-- insert YouTube video -->
