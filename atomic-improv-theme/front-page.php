ddd0255780630c58beae75eef5ec46897a95c4e5
Jen <47681735+j-soule@users.noreply.github.com>
2024-03-31 11:39:44 -0600

    Adding neon glow hover states

--- atomic-improv-theme/front-page.php
+++ atomic-improv-theme/front-page.php
@@ -9,25 +9,23 @@ get_header();
 
 <main id="primary" class="site-main">
 
-    <div class="full-screen-video-container">
-      <video autoplay loop muted>
-        <source
-          src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/atomic-ad-2.mp4"
-          type="video/mp4">
-      </video>
-      <!-- <div class="vid">
+  <div class="full-screen-video-container">
+    <video autoplay loop muted>
+      <source src="http://atomic-improv-a.web.dmitcapstone.ca/wp-content/themes/atomic-improv-theme/images/atomic-ad-2.mp4" type="video/mp4">
+    </video>
+    <!-- <div class="vid">
           <iframe width="1280" height="720" src="https://www.youtube.com/embed/Vkwthc6cyBY" title="Atomic Ad"
             frameborder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             allowfullscreen></iframe>
         </div> -->
 
-      <div class="full-screen-video-content">
-        <h1>Atomic<br><span>Improv</span></h1>
-        <p>Canada's premier improv comedy team</p>
-        <a href="contact-us" >Book Now</a>
-      </div>
+    <div class="full-screen-video-content">
+      <h1>Atomic<br><span>Improv</span></h1>
+      <p>Canada's premier improv comedy team</p>
+      <a href="contact-us">Book Now</a>
     </div>
+  </div>
 
   <section class="frontpage-top">
     <h1>Atomic Improv!</h1>
@@ -40,19 +38,11 @@ get_header();
         <div class="full-screen-video-content">
           <h2>Atomic <span>Improv</span></h2>
           <p>Canada's premier improv comedy team</p>
-          <a href="contact-us" >Book Now</a>
+          <a href="contact-us">Book Now</a>
         </div>
       </div>
     </div>
   </section><!-- end of bg-video -->
-
-
-
-
-
-
-
-
 
   <!-- What We Do  -->
   <section class="what-we-do">
@@ -62,12 +52,12 @@ get_header();
         <?php
         $whatwedo_query = new WP_Query(array("post_type" => "image", "posts_per_page" => 1, "tag" => "what-we-do"));
         ?>
-        <?php if ($whatwedo_query->have_posts()): ?>
+        <?php if ($whatwedo_query->have_posts()) : ?>
           <?php
-          while ($whatwedo_query->have_posts()):
+          while ($whatwedo_query->have_posts()) :
             $whatwedo_query->the_post();
             $whatwedo_url = get_field("image");
-            ?>
+          ?>
             <a href="what-we-do"><img src="<?php echo $whatwedo_url ?>"></a>
           <?php endwhile;
           wp_reset_postdata(); ?>
@@ -82,10 +72,10 @@ get_header();
       <div class="section-content">
         <h3>Customized shows for your events</h3>
         <p>Atomic Improv is Canada's Premier two man Improv troupe that has been
-      creating comedy on the spot based entirely on audience suggestion since 1990.</p>
-      <p>These 3 time World Improv champions and main stage performer at Montreal's just for laughs can help turn your event from humdrum to hilarious!</p>
+          creating comedy on the spot based entirely on audience suggestion since 1990.</p>
+        <p>These 3 time World Improv champions and main stage performer at Montreal's just for laughs can help turn your event from humdrum to hilarious!</p>
 
-          
+
         <a href="what-we-do">Learn More</a>
       </div>
 
@@ -96,12 +86,12 @@ get_header();
         <?php
         $powerofyes_query = new WP_Query(array("post_type" => "image", "posts_per_page" => 1, "tag" => "power-of-yes"));
         ?>
-        <?php if ($powerofyes_query->have_posts()): ?>
+        <?php if ($powerofyes_query->have_posts()) : ?>
           <?php
-          while ($powerofyes_query->have_posts()):
+          while ($powerofyes_query->have_posts()) :
             $powerofyes_query->the_post();
             $powerofyes_url = get_field("image");
-            ?>
+          ?>
             <a href="what-we-do"><img src="<?php echo $powerofyes_url ?>"></a>
           <?php endwhile;
           wp_reset_postdata(); ?>
@@ -136,12 +126,12 @@ get_header();
         <?php
         $whoweare_query = new WP_Query(array("post_type" => "image", "posts_per_page" => 1, "tag" => "who-we-are"));
         ?>
-        <?php if ($whoweare_query->have_posts()): ?>
+        <?php if ($whoweare_query->have_posts()) : ?>
           <?php
-          while ($whoweare_query->have_posts()):
+          while ($whoweare_query->have_posts()) :
             $whoweare_query->the_post();
             $whoweare_url = get_field("image");
-            ?>
+          ?>
             <a href="who-we-are"><img src="<?php echo $whoweare_url ?>"></a>
           <?php endwhile;
           wp_reset_postdata(); ?>
@@ -154,9 +144,9 @@ get_header();
         </div>
       </div>
       <div class="section-content">
-      <p>Atomic Improv is Canada's premier two man improv troupe that has been
-      creating comedy on the spot based entirely on audience suggestion since 1990.</p>
-      <p>These 3-time World Improv Champions and main stage performer at Montreal's Just For Laughs can help turn your event from humdrum to hilarious!</p>
+        <p>Atomic Improv is Canada's premier two man improv troupe that has been
+          creating comedy on the spot based entirely on audience suggestion since 1990.</p>
+        <p>These 3-time World Improv Champions and main stage performer at Montreal's Just For Laughs can help turn your event from humdrum to hilarious!</p>
 
         <a href="who-we-are">More About Us</a>
       </div>
@@ -193,12 +183,12 @@ get_header();
             );
             $instagram_query = new WP_Query($instagram_args);
             // Tests if there even are any posts
-            if ($instagram_query->have_posts()):
+            if ($instagram_query->have_posts()) :
               // As long as there are new posts...
-              while ($instagram_query->have_posts()):
+              while ($instagram_query->have_posts()) :
                 // Select the next post
                 $instagram_query->the_post();
-                ?>
+            ?>
                 <h4>
                   <?php the_title(); ?>
                 </h4>
@@ -206,7 +196,7 @@ get_header();
 
               <?php endwhile; ?>
               <!-- end loop -->
-              <?php
+            <?php
             endif;
             wp_reset_postdata();
             ?>
@@ -225,12 +215,12 @@ get_header();
             );
             $facebook_query = new WP_Query($facebook_args);
             // Tests if there even are any posts
-            if ($facebook_query->have_posts()):
+            if ($facebook_query->have_posts()) :
               // As long as there are new posts...
-              while ($facebook_query->have_posts()):
+              while ($facebook_query->have_posts()) :
                 // Select the next post
                 $facebook_query->the_post();
-                ?>
+            ?>
                 <h4>
                   <?php the_title(); ?>
                 </h4>
@@ -238,7 +228,7 @@ get_header();
 
               <?php endwhile; ?>
               <!-- end loop -->
-              <?php
+            <?php
             endif;
             wp_reset_postdata();
             ?>
@@ -257,12 +247,12 @@ get_header();
             );
             $youtube_query = new WP_Query($youtube_args);
             // Tests if there even are any posts
-            if ($youtube_query->have_posts()):
+            if ($youtube_query->have_posts()) :
               // As long as there are new posts...
-              while ($youtube_query->have_posts()):
+              while ($youtube_query->have_posts()) :
                 // Select the next post
                 $youtube_query->the_post();
-                ?>
+            ?>
                 <h4>
                   <?php the_title(); ?>
                 </h4>
@@ -270,7 +260,7 @@ get_header();
 
               <?php endwhile; ?>
               <!-- end loop -->
-              <?php
+            <?php
             endif;
             wp_reset_postdata();
             ?>
